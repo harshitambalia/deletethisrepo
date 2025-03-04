fa0b93bc9718bafa5ae455544a06f72885d71d75
airflow <airflow@78f29f609cf0>
2025-03-04 10:09:16 +0000

    Updated Snowflake queries

---
+++ MY_DBT_EXPERIMENT_DB/MY_SCHEMA/FINAL_STUDENTS.sql
@@ -0,0 +1,8 @@
+create or replace TABLE FINAL_STUDENTS (
+	ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
+	FIRST_NAME VARCHAR(50),
+	LAST_NAME VARCHAR(50),
+	AGE NUMBER(38,0),
+	GRADE VARCHAR(2),
+	primary key (ID)
+);
